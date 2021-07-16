ac4ab94497c5216af43991c55627e1e726ac88be
VicD1999 <victordachet@gmail.com>
2021-07-16 19:35:37 +0200

    Split dataset train/valid/test

--- main.py
+++ main.py
@@ -5,60 +5,12 @@ import pandas as pd
 
 import util as u
 
+if __name__ == '__main__':
+    # u.create_dataset(vervose=False)
 
-paths_ores = ["data/ORES/export_eolien_2021-02-01.csv",
-              "data/ORES/export_eolien_2021-03-01.csv",
-              "data/ORES/export_eolien_2021-04-01.csv",
-              "data/ORES/export_eolien_2021-05-01.csv",
-              "data/ORES/export_eolien_2021-06-01.csv",
-              "data/ORES/export_eolien_2021-07-01.csv"]
+    df = pd.read_csv("data/dataset.csv")
 
-paths_mar = ["data/MAR/concat_20210118_20210131.nc", # 02
-             "data/MAR/concat_20210201_20210228.nc", # 03
-             "data/MAR/concat_20210228_20210331.nc", # 04
-             "data/MAR/concat_20210331_20210430.nc", # 05
-             "data/MAR/concat_20210430_20210531.nc", # 06
-             "data/MAR/concat_20210531_20210630.nc"] # 07
+    data = u.get_random_split_dataset(df)
 
-frames = []
-
-for path_ores, path_mar in zip(paths_ores, paths_mar):
-    print(path_ores, path_mar)
-    
-    print("Length frame: ", len(frames))
-    try:
-        dataset = u.create_dataframe(path_ores_data=path_ores, 
-                                     path_mar_data=path_mar)
-
-        frames.append(dataset)
-
-        
-        for i in range(3):
-            print("Wind farm ", i)
-
-            correlation = np.corrcoef(dataset[f'windSpeedNorm{i}'], 
-                                      dataset[f'prod_wf{i}'])
-
-            print("Correlation between wind speed and production:\n", correlation)
-
-            spearman = stats.spearmanr(dataset[f'windSpeedNorm{i}'], 
-                                       dataset[f'prod_wf{i}'])
-
-            print("Spearman coefficient between wind speed and production:\n", 
-                  spearman.correlation)
-
-            print("\n")
-        
-    except:
-        print("Error with ", paths_ores, path_mar)
-
-
-dataset = pd.concat(frames)
-dataset = dataset.drop_duplicates(subset = "time0")
-dataset = dataset.drop_duplicates(subset = "time1")
-dataset = dataset.drop_duplicates(subset = "time2")
-
-# Save dataset
-dataset.to_csv("data/dataset.csv")
-
+    u.write_split_dataset(data)
     
--- util.py
+++ util.py
@@ -4,6 +4,22 @@ import numpy as np
 from scipy.io import netcdf
 import pandas as pd
 import datetime
+import random
+import pickle
+
+paths_ores = ["data/ORES/export_eolien_2021-02-01.csv",
+              "data/ORES/export_eolien_2021-03-01.csv",
+              "data/ORES/export_eolien_2021-04-01.csv",
+              "data/ORES/export_eolien_2021-05-01.csv",
+              "data/ORES/export_eolien_2021-06-01.csv",
+              "data/ORES/export_eolien_2021-07-01.csv"]
+
+paths_mar = ["data/MAR/concat_20210118_20210131.nc", # 02
+             "data/MAR/concat_20210201_20210228.nc", # 03
+             "data/MAR/concat_20210228_20210331.nc", # 04
+             "data/MAR/concat_20210331_20210430.nc", # 05
+             "data/MAR/concat_20210430_20210531.nc", # 06
+             "data/MAR/concat_20210531_20210630.nc"] # 07
 
 def get_wind_time(netCDF_file_path, location):
     """
@@ -122,6 +138,9 @@ def create_dataframe(path_ores_data, path_mar_data):
 
     ores_df = pd.read_csv(path_ores_data, header=None, names=names)
 
+    power_installed = ores_df.sort_values(by="coord",
+                        axis=0)['rated installed power (kVA)'].unique()
+
     # Correction DataFrame for corrupted files
     if path_ores_data in paths_corrupted:
         ores_df = ores_df.set_index(np.arange(0,len(ores_df),1))
@@ -142,7 +161,8 @@ def create_dataframe(path_ores_data, path_mar_data):
         print(f"(i, location) = ({i}, {location})")
         time_serie = prod_per_wind_farm.iloc[i] # Get prod for i th wind farm
 
-        uuz, vvz, time, start_time = get_wind_time(netCDF_file_path=path_mar_data, 
+        uuz, vvz, time, start_time = get_wind_time(netCDF_file_path=\
+                                                                path_mar_data, 
                                                    location=location)
 
         time = [convert_min_to_date(start_time, elapsed_minutes) 
@@ -154,7 +174,8 @@ def create_dataframe(path_ores_data, path_mar_data):
 
         # Interpolation part
         # Wind Speed X
-        points = [convert_date_to_min(date=str(date)) for date in netcdf_df['time'][:]]
+        points = [convert_date_to_min(date=str(date)) \
+                    for date in netcdf_df['time'][:]]
         values = netcdf_df['uuz'][:]
 
         interp_x = [convert_date_to_min(date=date[:-1]) 
@@ -163,7 +184,8 @@ def create_dataframe(path_ores_data, path_mar_data):
         interp_speedX = np.interp(interp_x, points, values)
 
         # Wind Speed Y
-        points = [convert_date_to_min(date=str(date)) for date in netcdf_df['time'][:]]
+        points = [convert_date_to_min(date=str(date)) \
+                    for date in netcdf_df['time'][:]]
         values = netcdf_df['vvz'][:]
 
         interp_y = [convert_date_to_min(date=date[:-1]) 
@@ -176,14 +198,261 @@ def create_dataframe(path_ores_data, path_mar_data):
         norm_ws = np.sqrt(np.square(interp_speedX) + np.square(interp_speedY))
 
         # Dataset Creation
-        dataset = dataset + [(f"prod_wf{i}",time_serie.values), 
-                             (f"windSpeedNorm{i}", norm_ws),
-                             (f"time{i}", time_serie.index.values)]
+        dataset = dataset + [(f"prod_wf{i}", 
+                              time_serie.values / power_installed[i]), 
+                             (f"windSpeedNorm{i}", norm_ws)]
+
+    dataset = dataset + [(f"time", time_serie.index.values)]
 
     # convert list into dictionary to construct the DataFrame   
     dataset = {key:value for key, value in dataset}
         
     df = pd.DataFrame(dataset)
-    
+
     return df
+
+def create_dataset(vervose=True):
+    """
+    Routine to create a dataset from netcdf and ORES files
+
+    All the paths must be given in the global variables "path_ores"
+    and "path_mar" in the head of this file
+
+    """
+
+    frames = []
+
+    for path_ores, path_mar in zip(paths_ores, paths_mar):
+        print(path_ores, path_mar)
+        
+        print("Length frame: ", len(frames))
+        try:
+            dataset = create_dataframe(path_ores_data=path_ores, 
+                                         path_mar_data=path_mar)
+
+            frames.append(dataset)
+
+            
+            for i in range(3):
+                correlation = np.corrcoef(dataset[f'windSpeedNorm{i}'], 
+                                          dataset[f'prod_wf{i}'])
+
+                spearman = stats.spearmanr(dataset[f'windSpeedNorm{i}'], 
+                                           dataset[f'prod_wf{i}'])
+
+                if vervose:
+                    print("Wind farm ", i)
+                    print("Correlation between wind speed and production:\n", 
+                          correlation)
+                    print("Spearman coefficient between wind speed and \
+                          production:\n", 
+                          spearman.correlation)
+                    print("\n")
+            
+        except:
+            print("Error with ", paths_ores, path_mar)
+
+
+    dataset = pd.concat(frames)
+
+    dataset = dataset.drop_duplicates(subset = "time")
+
+    # Add sin and cosinus minutes of a day
+    vector_seconds = [datetime.timedelta(hours=int(ele[11:-7]), 
+                      minutes=int(ele[14:-4]), 
+                      seconds=int(ele[17:-1])).seconds \
+                      for ele in dataset[f"time"] ]
+
+    f_sin = lambda ele: np.sin(2*np.pi*(ele)/(24*60))
+    f_cos = lambda ele: np.cos(2*np.pi*(ele)/(24*60))
+
+    dataset[f"sin_" + f"time"] = [f_sin(ele) for ele in vector_seconds]
+    dataset[f"cos_" + f"time"] = [f_cos(ele) for ele in vector_seconds]
+
+    # Save dataset
+    dataset.to_csv("data/dataset.csv", index=False)
+    print("Dataset saved")
+
+
+def feature_label_split(df, window_size, forecast_size, 
+                        features, verbose=False):
+    """
+    
+    args:
+    -----
+        df: DataFrame
+        window_size: int
+                     Size of the input window
+        forecast_size: int
+                       Size of the forecasted window
+        features: list of Strings 
+                  containing the name of the features
+        verbose: bool
+                 if True print added
+    return:
+    -------
+        X: features matrix of size (n_samples, window_size, n_features)
+        y: target values of size (n_samples, forecast_size)
+    
+    """
+    
+    
+    tot_window = window_size + forecast_size
+    
+    assert tot_window < len(df), "Error"
+    
+    step = 1
+    
+    n_samples = len(df) // step
+    
+    X = np.zeros((n_samples, window_size, len(features)))
+    y = np.zeros((n_samples, forecast_size))
+    
+    for i in range(0, len(df)-tot_window, step):
+        if i % 100_000 == 0 and verbose:
+            print(round(i/(len(df)-tot_window),2))
+            
+        features_values = []
+        
+        for feature in features:
+            features_values.append(df[feature][i:i+window_size].values)
+            
+        features_values = np.array(features_values).T
+            
+        X[i // step,:,:] = features_values
+        y[i // step,:] = df["windSpeedNorm0"]\
+                           [i+window_size:i+window_size+forecast_size].values
+        
+    
+    return X, y
+
+
+def get_random_split_dataset(df, num_bins=50, percentage=0.2, window_size=120, 
+                             forecast_size=60, 
+                             features=["windSpeedNorm0", "sin_time", "cos_time"]):
+    """
+    args:
+    -----
+        df: DataFrame 
+            with power production from windfarms
+        num_bins: int
+                  Number of splits made in the dataset
+        percentage: float
+                    proportion of bins to use for the validation and test set
+        window_size: int
+                     size of the input window
+        forecast_size: int
+                       size of the forecast window
+        features: list of Strings 
+                  containing the name of the features
+    return:
+    -------
+        split_data: Dictionary
+                    containing the fields X_train, X_valid, X_test which 
+                    correspond to 3D numpy array with input windows of size:
+                    (n_samples, window_size, n_features)
+                    
+                    and
+                    containing y_train, y_valid, y_test which correspond 
+                    to 2D numpy array of the forecasting window of size:
+                    (n_samples, forecast_size)
+
+                    
+    """
+    random.seed(0)
+
+    train_set = []
+    valid_set = []
+    test_set = []
+
+    for i in range(int(0.5 * percentage * num_bins)):
+        tmp = random.randint(0,num_bins-1)
+        if tmp not in valid_set:
+            valid_set.append(tmp)
+    
+    i=0
+    while i < int(0.5 * percentage * num_bins):
+        tmp = random.randint(0,num_bins-1)
+        
+        if tmp not in valid_set and tmp not in test_set:
+            test_set.append(tmp)
+            i+=1
+    
+    test_valid_set = valid_set + test_set
+    train_set = [ele for ele in range(num_bins) if ele not in test_valid_set]
+    
+    # endpoint=False allows to not have the last indices of the dataframe
+    indices = np.linspace(0, len(df), num_bins, endpoint=False).astype(int)
+        
+    train_set = indices[train_set]
+    valid_set = indices[valid_set]
+    test_set = indices[test_set]
+    
+    # Second Part to create the window datasets
+    n_features = len(features)
+    train_size = int((1 - percentage) * len(df))
+    
+    X_train = None
+    y_train = None
+    
+    X_valid = None
+    y_valid = None
+    
+    X_test = None
+    y_test = None
+    
+    for i in range(num_bins):
+        print(f"Part {i} over {num_bins}")
+        
+        if i == num_bins - 1:
+            df_to_feature = df.iloc[indices[i]:]
+        else:
+            df_to_feature = df.iloc[indices[i]:indices[i+1]]
+        X, y = feature_label_split(df=df_to_feature, 
+                                   window_size=window_size, 
+                                   forecast_size=forecast_size, 
+                                   features=features) 
+
+            
+        if indices[i] in train_set:
+            if X_train is None and y_train is None:
+                X_train = X
+                y_train = y
+            else:
+                X_train = np.concatenate((X_train, X), axis=0)
+                y_train = np.concatenate((y_train, y), axis=0)
+                
+        elif indices[i] in valid_set:
+            if X_valid is None and y_valid is None:
+                X_valid = X
+                y_valid = y
+            else:
+                X_valid = np.concatenate((X_valid, X), axis=0)
+                y_valid = np.concatenate((y_valid, y), axis=0)
+        else:
+            if X_test is None and y_test is None:
+                X_test = X
+                y_test = y
+            else:
+                X_test = np.concatenate((X_test, X), axis=0)
+                y_test = np.concatenate((y_test, y), axis=0)
+    
+    # Problem with the last 180 items I do not catch the error yet
+    error = window_size + forecast_size
+    split_data = {"X_train":X_train[:-error,:,:], "y_train":y_train[:-error], 
+                  "X_valid":X_valid[:-error,:,:], "y_valid":y_valid[:-error],
+                  "X_test":X_test[:-error,:,:], "y_test":y_test[:-error]}
+    
+    return split_data
+
+def load_split_dataset(path="data/data.pkl"):
+    with open(path, "rb") as f:
+        data = pickle.load(f)
+
+    return data
+
+def write_split_dataset(data, path="data/data.pkl"):
+    with open(path, "wb") as f:
+        pickle.dump(data, f)
+
 
