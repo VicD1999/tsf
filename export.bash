d9c5451fc82b9c92ca80fa8cf1c4c2c2c1868be1
VicD1999 <victordachet@gmail.com>
2021-07-20 12:02:06 +0200

    Password not needed anymore

--- export.bash
+++ export.bash
@@ -2,4 +2,4 @@
 
 zip archive.zip main.py model.py util.py
 
-scp archive.zip victor@vega.montefiore.ulg.ac.be:/home/victor/tsf
+scp -i ~/.ssh/vega archive.zip victor@vega.montefiore.ulg.ac.be:/home/victor/tsf
