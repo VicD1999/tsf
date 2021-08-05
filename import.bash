18448d7fa3cf5146349ebff7eb40e515d94cf31a
VicD1999 <victordachet@gmail.com>
2021-08-05 23:20:15 +0200

    Change in paths

--- import.bash
+++ import.bash
@@ -3,7 +3,7 @@
 echo "Fisrt agument: name of the model"
 echo "Second agument: number of the model"
 
-scp -i ~/.ssh/vega victor@vega.montefiore.ulg.ac.be:/home/victor/tsf/results/*.csv /Users/victordachet/Documents/Projet_Ernst_Fettweis/2021/tsf/results
+scp -i ~/.ssh/vega victor@vega.montefiore.ulg.ac.be:/home/victor/tsf/results/*.csv results
 
-scp -i ~/.ssh/vega victor@vega.montefiore.ulg.ac.be:/home/victor/tsf/model/$1/$2.model /Users/victordachet/Documents/Projet_Ernst_Fettweis/2021/tsf/model/$1
+scp -i ~/.ssh/vega victor@vega.montefiore.ulg.ac.be:/home/victor/tsf/model/$1/$2.model model/$1
 
