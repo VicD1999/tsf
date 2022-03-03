5f3d7a3463b110fc2d4e1c96bb9da1a3520eb4b3
Dachet Victor <victordachet@gmail.com>
2022-03-03 16:43:31 +0100

    Fill NA

--- gefcom.py
+++ gefcom.py
@@ -4,65 +4,69 @@ import matplotlib.pyplot as plt
 
 
 def create_gefcom_dataset():
-	dico = {}
+    dico = {}
 
-	for zone in range(1,11):
-	    df = pd.DataFrame()
-	    for i in range(1,16):
-	        # print(i)
-	        df = pd.concat([df, pd.read_csv(f"GEFCom2014Data/Wind/Task {i}/Task{i}_W_Zone1_10/Task{i}_W_Zone{zone}.csv")], ignore_index=True)
-	        # print(df.iloc[-1])
-	        print()
+    for zone in range(1,11):
+        df = pd.DataFrame()
+        for i in range(1,16):
+            # print(i)
+            df = pd.concat([df, pd.read_csv(f"GEFCom2014Data/Wind/Task {i}/Task{i}_W_Zone1_10/Task{i}_W_Zone{zone}.csv")], ignore_index=True)
+            # print(df.iloc[-1])
+            # print()
 
-	    df = df.drop_duplicates(subset=["TIMESTAMP"],  ignore_index=True)
-	        
-	    dico[zone] = df.copy()
+        df = df.drop_duplicates(subset=["TIMESTAMP"],  ignore_index=True)
+            
+        dico[zone] = df.copy()
 
-	# COncatenation of the zones
-	df = pd.concat([dico[i] for i in range(1,11)])
+    # COncatenation of the zones
+    df = pd.concat([dico[i] for i in range(1,11)])
+    df.fillna(method="ffill", inplace=True)
 
-	df.to_csv("GEFCom2014Data/Wind/concat.csv")
+    df.to_csv("GEFCom2014Data/Wind/concat.csv")
 
 def open_gefcom():
-	df = pd.read_csv("GEFCom2014Data/Wind/concat.csv")
-	df.rename(columns={"Unnamed: 0":"t"})
+    df = pd.read_csv("GEFCom2014Data/Wind/concat.csv")
+    df.rename(columns={"Unnamed: 0":"t"})
 
-	return df
+    return df
 
 def get_split_dataset(df):
-	"""
-	args: df: DataFrame 
-	-----
+    """
+    args: df: DataFrame 
+    -----
 
-	"""
+    """
 
-	split_train = "20130801 00:00"
-	split_valid = "20131001 00:00"
+    split_train = "20130801 00:00"
+    split_valid = "20131001 00:00"
 
-	train_set = df[df["TIMESTAMP"] < split_train]
-	condition = df["TIMESTAMP"] >=  split_train
-	condition2 = df["TIMESTAMP"] < split_valid
-	valid_set = df.loc[(condition & condition2)]
-	test_set = df[df["TIMESTAMP"] >= split_valid]
+    train_set = df[df["TIMESTAMP"] < split_train]
+    condition = df["TIMESTAMP"] >=  split_train
+    condition2 = df["TIMESTAMP"] < split_valid
+    valid_set = df.loc[(condition & condition2)]
+    test_set = df[df["TIMESTAMP"] >= split_valid]
 
 
-	return train_set, valid_set, test_set
+    return train_set, valid_set, test_set
 
 
 
 if __name__ == '__main__':
+    create_gefcom_dataset()
 
-	df = open_gefcom()
+    df = open_gefcom()
 
-	train_set, valid_set, test_set = get_split_dataset(df=df)
+    print(df['TARGETVAR'].isnull().sum().sum())
 
-	print(train_set)
-	print()
+    train_set, valid_set, test_set = get_split_dataset(df=df)
 
-	print(valid_set)
-	print()
+    print(train_set)
+    print()
 
-	print(test_set)
-	print()
+    print(valid_set)
+    print()
+
+    print(test_set)
+    print()
 
 
