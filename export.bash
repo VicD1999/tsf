328d88b841cc105170532491e3bd89f3bc5b4805
Dachet Victor <victordachet@gmail.com>
2022-04-11 13:37:56 +0200

    Orga

--- export.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-zip archive.zip main.py model.py util.py attention.py randomForest.py gradient_boosting.py
-
-scp -i ~/.ssh/vegamissile archive.zip victor@vega.mont.priv:/home/victor/tsf
