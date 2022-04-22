46db568a751e47d2380577a9eb7f670f5d10d053
Dachet Victor <victordachet@gmail.com>
2022-04-22 11:24:52 +0200

    Forward pass working

--- python_code/transformer.py
+++ python_code/transformer.py
@@ -1,48 +1,94 @@
 import torch
 import torch.nn as nn
 
+print("Hello")
 
-class Transformer(nn.Module):
-    # d_model : number of features
-    def __init__(self,feature_size=5,num_layers=3,seq_length=120, output_size=60 ,dropout=0):
-        super(Transformer, self).__init__()
+import math
+from typing import Tuple
 
-        self.encoder_layer = nn.TransformerEncoderLayer(d_model=feature_size, nhead=feature_size, dropout=dropout, batch_first=True)
-        self.transformer_encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=num_layers)        
-        self.decoder = nn.Linear(feature_size,1)
-        self.out = nn.Linear(seq_length, output_size)
+import torch
+from torch import nn, Tensor
+import torch.nn.functional as F
+from torch.nn import TransformerEncoder, TransformerEncoderLayer
+from torch.utils.data import dataset
+
+class TransformerModel(nn.Module):
+
+    def __init__(self, d_model: int, nhead: int, d_hid: int,
+                 nlayers: int, dropout: float = 0.5):
+        super().__init__()
+        self.model_type = 'Transformer'
+        self.pos_encoder = PositionalEncoding(d_model, dropout)
+        encoder_layers = TransformerEncoderLayer(d_model, nhead, d_hid, dropout)
+        self.transformer_encoder = TransformerEncoder(encoder_layers, nlayers)
+        # self.encoder = nn.Embedding(ntoken, d_model)
+        self.d_model = d_model
+        self.decoder = nn.Linear(d_model, 96)
+
         self.init_weights()
 
-    def init_weights(self):
-        initrange = 0.1    
+    def init_weights(self) -> None:
+        initrange = 0.1
+        # self.encoder.weight.data.uniform_(-initrange, initrange)
         self.decoder.bias.data.zero_()
         self.decoder.weight.data.uniform_(-initrange, initrange)
 
-    def _generate_square_subsequent_mask(self, sz):
-        # print("sz", sz.shape)
-        mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
-        print("mask", mask.shape)
-        mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
-        print("mask", mask.shape)
-        return mask
+    def forward(self, src: Tensor, src_mask: Tensor) -> Tensor:
+        """
+        Args:
+            src: Tensor, shape [seq_len, batch_size]
+            src_mask: Tensor, shape [seq_len, seq_len]
 
-    def forward(self, src):
-        
-        mask = self._generate_square_subsequent_mask(src.shape[1]) # .to(device)
-        print("mask", mask.shape)
+        Returns:
+            output Tensor of shape [seq_len, batch_size, ntoken]
+        """
+        print("src before sqrt", src.shape)
+        src = src * math.sqrt(self.d_model)
+        print("src AFter sqrt", src.shape)
+        src = self.pos_encoder(src.unsqueeze(2))
         print("src", src.shape)
-        output = self.transformer_encoder(src, mask)
-        print("Trasnf encoder output", output.shape)
+        print("src_mask", src_mask.shape)
+        output = self.transformer_encoder(src, src_mask)
         output = self.decoder(output)
-        print("Decoder output", output.shape)
-        output = self.out(output[:,:,0])
-        print("out output", output.shape)
         return output
 
+
+def generate_square_subsequent_mask(sz: int) -> Tensor:
+    """Generates an upper-triangular matrix of -inf, with zeros on diag."""
+    return torch.triu(torch.ones(sz, sz) * float('-inf'), diagonal=1)
+
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+        pe = torch.zeros(max_len, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        Args:
+            x: Tensor, shape [seq_len, batch_size, embedding_dim]
+        """
+        # print("PositionalEncoding x", x.shape)
+        x = x + self.pe[:x.size(0)]
+        return self.dropout(x)
+
+
 if __name__ == "__main__":
-	model = Transformer()
-	x = torch.rand((32, 120, 5))
-	print("x", x.shape)
-	output = model(x)
-	print("output", output.shape)
+    print("Yo")
+    model = TransformerModel(d_model=512, nhead=8, d_hid=2048,
+                        nlayers=3, dropout=0.5)
+    print("Hello")
+    x = torch.rand((120, 32))
+    print("x", x.shape)
+    src_mask = generate_square_subsequent_mask(120) # .to(device)
+    output = model(x, src_mask)
+    print("output", output.shape)
     
